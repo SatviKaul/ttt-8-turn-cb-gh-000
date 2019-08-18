6ac01885271c45465ade4c9fb39754b696fe3e2a
Satvik Kaul <satvik.1799@gmail.com>
2019-08-18 10:24:45 +0000

    Automatically backed up by Learn

--- lib/help.rb
+++ lib/help.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/turn.rb'
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets
+  input = gets.strip
   input_to_index(input)
   if input.between?(0,8) && !position_taken?(board, input)
     move(board, input)
